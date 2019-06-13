d2dfd2a33c822e58892e4db8c039f75f666eb087
EC2 Default User <ec2-user@ip-172-31-20-118.us-east-2.compute.internal>
2019-06-13 20:05:46 +0000

    initialize repository

---
+++ index.js
@@ -0,0 +1,40 @@
+String.prototype.reverse = function reverse() {
+  return Array.from(this).reverse().join("");
+};
+
+function Phrase(content) {
+  this.content = content;
+
+  this.processor = function() {
+    return this.content.toLowerCase();
+  };
+  
+  this.processedContent = function processedContent() {
+    return this.processor(this.content);
+  };
+
+  // Returns true if the phrase is a palindrome, false otherwise.
+  this.palindrome = function palindrome() {
+    return this.processedContent() === this.processedContent().reverse();
+  };
+}
+
+function TranslatedPhrase(content, translation) {
+  this.content = content;
+  this.translation = translation;
+
+  // Returns translation processed for palindrome testing.
+  this.processedContent = function processedContent() {
+    return this.processor(this.translation);
+  };
+}
+
+TranslatedPhrase.prototype = new Phrase();
+
+String.prototype.blank = function() {
+    return !!this.match(/^\s*$/g);
+};
+
+Array.prototype.last = function() {
+    return this[this.length -1];
+};
